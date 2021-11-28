307f8ea5d0b165d3ee1e5c16b731a801dab2d4b5
Chinmay-395 <chinmaydali395@gmail.com>
2021-11-28 13:07:50 +0530

    Added new XP and scrollbar fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import Sidebar from './components/Sidebar'
-import Navbar from './components/Navbar';
-import { Route, Redirect, Switch, useLocation } from 'react-router-dom';
-import About from './components/about/About';
-import Resume from './components/Resume';
-import Projects from './components/Projects';
-import { AnimatePresence } from 'framer-motion'
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import { Route, Redirect, Switch, useLocation } from "react-router-dom";
+import About from "./components/about/About";
+import Resume from "./components/Resume";
+import Projects from "./components/Projects";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
-
   const location = useLocation();
   return (
-
     <div className="app">
       <div className="container app__container">
         <div className="row app__row">
           <div className="col-lg-3">
-
             <Sidebar />
-
           </div>
-          <div className="col-lg-9 app__main-content">
+          <div className="col-lg-9 app__main-content" id="internal_scrollbar">
             {/* navbar */}
             <Navbar />
 
@@ -42,7 +38,6 @@ function App() {
         </div>
       </div>
     </div>
-
   );
 }
 
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -33,25 +33,54 @@ const Resume = () => {
       initial="hidden"
       animate="visible"
       exit="exit"
+      id="overflow_check"
     >
       <div className="row">
-        <div className="col-lg-6 resume-card">
+        <div className="col-lg-12 resume-card">
           <h4 className="resume-card__heading">Education</h4>
           <div className="resume-card__body">
-            <h5 className="resume-card__title">
-              Information Technology Enginnering
-            </h5>
+            <h5 className="resume-card__title">BE in Information Technology</h5>
             <p className="resume-card__name">Mumbai University(2017-2021)</p>
-            <p className="resume-card__details">
+            {/* <p className="resume-card__details">
               I am currently persuing B.E in Information Technology Engineering
               Thakur College of Engineering and Technology
-            </p>
+            </p> */}
           </div>
         </div>
-        <div className="col-lg-6 resume-card">
+        <div className="col-lg-12 resume-card">
           <h4 className="resume-card__heading">Experience</h4>
           <div className="resume-card__body">
-            <h5 className="resume-card__title">Intern</h5>
+            <h5 className="resume-card__title">Software Engineer</h5>
+            <p className="resume-card__name">
+              ONETURE TECHNOLOGIES Aug 2021 - Present
+            </p>
+            <p className="resume-card__details">
+              • Worked closely with customer support to respond quickly to user
+              issues, permanently solve them, and reduce the turnaround time to
+              fixes. <br />
+              • Implemented new C# class library for SQL server database access
+              layer and updated previous web page frameworks <br />• Tech Stack:
+              C#, ASP.NET, XML, SOAP, HTTP, SQL, MS SQL Server, Reactjs Angular,
+              Typescript, Jquery, Azure
+            </p>
+          </div>
+          <div className="resume-card__body">
+            <h5 className="resume-card__title">Software Engineer</h5>
+            <p className="resume-card__name">
+              NUZPAPR TECHNOLOGIES May 2021 - Aug 2021
+            </p>
+            <p className="resume-card__details">
+              • Developed a web app that gives authenticated employees access to
+              an anonymous forum and community where they may talk about
+              problems. The platform’s features include OAuth implementation,
+              checking whether a debate is open or closed, a likes counter, and
+              user points. Middleware authorization, e-mail and in-app push
+              notification <br />• Tech Stack: Mongodb, Expressjs, Nodejs,
+              Nextjs, Reactjs, redux, websockets, AWS, cypressio
+            </p>
+          </div>
+          <div className="resume-card__body">
+            <h5 className="resume-card__title">RPA Developer intern</h5>
             <p className="resume-card__name">XLNC Technologies (2019)</p>
             <p className="resume-card__details">
               I built a automated bot that would scrape all the articles related
